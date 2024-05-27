b903916753e9ebb2d8b349f22f7908dc35b58ed9
Gennadiy Shpak <gena_shpak@ukr.net>
2024-05-27 03:11:12 +0300

    Add_ulpload_pfoto_serch_contact_etc

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI, Form, HTTPException, Header, Request, Depends, status
+from datetime import datetime
+from fastapi import (
+    FastAPI,
+    File,
+    Form,
+    HTTPException,
+    Header,
+    Request,
+    Depends,
+    UploadFile,
+    # requests,
+    status,
+)
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from starlette.middleware.sessions import SessionMiddleware
 import httpx
 import uvicorn
+import requests
 
 app = FastAPI()
 
@@ -13,12 +27,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Підключення папки з шаблонами Jinja2
 templates = Jinja2Templates(directory="templates")
 
-base_url = (
-    "https://photoshare-python-back.onrender.com/api"  # URL локального сервера FastAPI
-)
+# base_url = "https://photoshare-python-back.onrender.com/api"
+base_url = "http://localhost:8000/api"
 
+app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
 
-ACCESS_TOKEN = None
 
 # Базовий шаблон з кнопками для запуску різних функцій
 @app.get("/", response_class=HTMLResponse)
@@ -54,7 +67,6 @@ async def signup_user(
                 "registration_success.html",
                 {"request": request, "data": response_data},
             )
-            # return {"message": "User registered successfully", "data": response.json()}
         except httpx.HTTPStatusError as e:
             if e.response.status_code == 422:
                 error_detail = e.response.json().get("detail", "Unprocessable Entity")
@@ -65,6 +77,7 @@ async def signup_user(
             return templates.TemplateResponse(
                 "registration_failure.html", {"request": request, "error": str(e)}
             )
+
 
 @app.get("/login", response_class=HTMLResponse)
 async def login_form(request: Request):
@@ -83,13 +96,43 @@ async def login_user(
             response.raise_for_status()
             tokens = response.json()
 
-            # Передача токенів через HTTP заголовок
-            ACCESS_TOKEN = tokens["access_token"]
-            headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
-            ##########################
-            # Виконати GET запит на /contacts з токеном авторизації
+            # ACCESS_TOKEN = tokens["access_token"]
+            request.session["access_token"] = tokens["access_token"]
+
+            return templates.TemplateResponse(
+                "login_success.html", {"request": request}
+            )
+
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 401:
+                return templates.TemplateResponse(
+                    "Unauthorized.html", {"request": request}
+                )
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Failed to fetch contacts: {e}",
+                )
+
+
+@app.get("/contacts", response_class=HTMLResponse)
+async def search_contacts(request: Request, limit: int = 10, offset: int = 0):
+    access_token = request.session.get("access_token")
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token missing or invalid",
+        )
+
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {"Authorization": f"Bearer {access_token}"}
+
             response_contacts = await client.get(
-                f"{base_url}/users/all", headers=headers, follow_redirects=True
+                f"{base_url}/users/all",
+                headers=headers,
+                params={"limit": limit, "offset": offset},
+                follow_redirects=True,
             )
             response_contacts.raise_for_status()
             contacts = response_contacts.json()
@@ -109,31 +152,168 @@ async def login_user(
                 )
 
 
-# Функція входу користувача
-@app.post("/contacts", response_class=HTMLResponse)
-async def search_contacts(
-    request: Request, name: str = Form(...), password: str = Form(...)
-):
-    data = {"username": name, "password": password}
+@app.get("/contact", response_class=HTMLResponse)
+async def search_contacts(request: Request):
+    access_token = request.session.get("access_token")
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token missing or invalid",
+        )
+
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.post(f"{base_url}/auth/login", data=data)
-            response.raise_for_status()
-            tokens = response.json()
+            headers = {"Authorization": f"Bearer {access_token}"}
 
-            # Передача токенів через HTTP заголовок
-            ACCESS_TOKEN = tokens["access_token"]
-            headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
-            ##########################
-            # Виконати GET запит на /contacts з токеном авторизації
             response_contacts = await client.get(
-                f"{base_url}/users/all", headers=headers, follow_redirects=True
+                f"{base_url}/users/me",
+                headers=headers,
+                # params={"limit": limit, "offset": offset},
+                follow_redirects=True,
             )
             response_contacts.raise_for_status()
-            contacts = response_contacts.json()
+            contact = response_contacts.json()
 
             return templates.TemplateResponse(
-                "contacts.html", {"request": request, "contacts": contacts}
+                "contact.html", {"request": request, "contact": contact}
+            )
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 401:
+                return templates.TemplateResponse(
+                    "Unauthorized.html", {"request": request}
+                )
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Failed to fetch contacts: {e}",
+                )
+
+
+@app.get("/edit_contact", response_class=HTMLResponse)
+async def login_form(request: Request):
+    return templates.TemplateResponse("/edit_contact.html", {"request": request})
+
+
+@app.post("/edit_contact", response_class=HTMLResponse)
+async def search_contacts(
+    request: Request,
+    username: str = Form(...),
+    phone: str = Form(...),
+    birthday: str = Form(...),
+):
+    try:
+        # Validate birthday format
+        birthday_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+
+        # Create the payload
+        data = {
+            "username": username,
+            "phone": phone,
+            "birthday": birthday_date.isoformat(),
+        }
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to update user: {e}",
+        )
+    access_token = request.session.get("access_token")
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token missing or invalid",
+        )
+
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {"Authorization": f"Bearer {access_token}"}
+
+            response_contact = await client.put(
+                f"{base_url}/users", headers=headers, json=data, follow_redirects=True
+            )
+            response_contact.raise_for_status()
+            contact = response_contact.json()
+
+            return templates.TemplateResponse(
+                "contact.html", {"request": request, "contact": contact}
+            )
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 401:
+                return templates.TemplateResponse(
+                    "Unauthorized.html", {"request": request}
+                )
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Failed to fetch contacts: {e}",
+                )
+
+
+@app.get("/upload_photo", response_class=HTMLResponse)
+async def upload_photo_form(request: Request):
+    return templates.TemplateResponse("upload_photo.html", {"request": request})
+
+
+@app.post("/upload_photo", response_class=HTMLResponse)
+async def upload_photo(
+    request: Request,
+    photo: UploadFile = File(...),
+    description: str = Form(...),
+    tags: str = Form(...),
+):
+    access_token = request.session.get("access_token")
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Access token missing or invalid")
+
+    # tags_list = tags.split(",")
+
+    async with httpx.AsyncClient() as client:
+
+        form_data = {
+            "description": description,
+            "tags": tags,
+        }
+
+        files = {"file": (photo.filename, photo.file, photo.content_type)}
+
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        response = requests.post(
+            f"{base_url}/photos",
+            headers=headers,
+            files=files,
+            data=form_data
+        )
+
+        
+        response.raise_for_status()
+
+    return templates.TemplateResponse("upload_success.html", {"request": request})
+
+
+@app.get("/photos", response_class=HTMLResponse)
+async def search_contacts(request: Request):
+    access_token = request.session.get("access_token")
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token missing or invalid",
+        )
+
+    async with httpx.AsyncClient() as client:
+        try:
+            headers = {"Authorization": f"Bearer {access_token}"}
+
+            response_contacts = await client.get(
+                f"{base_url}/photos",
+                headers=headers,
+                # params={"limit": limit, "offset": offset},
+                follow_redirects=True,
+            )
+            response_contacts.raise_for_status()
+            photos = response_contacts.json()
+
+            return templates.TemplateResponse(
+                "photos.html", {"request": request, "photos": photos}
             )
         except httpx.HTTPStatusError as e:
             if e.response.status_code == 401:
@@ -149,63 +329,3 @@ async def search_contacts(
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True, log_level="info")
-
-
-#############################
-#     return RedirectResponse(url="/contacts", headers=headers)
-#     # return templates.TemplateResponse("login.html",{"request": request, "message": f"Login successful {access_token}"},)
-# except httpx.HTTPStatusError as e:
-#     return templates.TemplateResponse(
-#         "login.html",
-#         {"request": request, "error_message": f"Login failed: {e}"},
-#     )
-
-
-# Функція отримання списку контактів
-# @app.get("/contacts", response_class=JSONResponse)
-# async def get_contacts(request: Request, authorization: str = Header(None)):
-#     if not authorization or not authorization.startswith("Bearer "):
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Invalid authorization header",
-#         )
-
-#     access_token = authorization.split(" ")[1]
-
-#     async with httpx.AsyncClient() as client:
-#         try:
-#             headers = {"Authorization": f"Bearer {access_token}"}
-#             response = await client.get(f"{base_url}/contacts/", headers=headers)
-#             response.raise_for_status()  # Перевірка статусу відповіді
-#             contacts = response.json()
-
-#             return contacts
-#             # return templates.TemplateResponse(
-#             #     "contacts.html", {"request": request, "contacts": contacts}
-#             # )
-#         except httpx.HTTPStatusError as e:
-#             raise HTTPException(
-#                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#                 detail=f"Failed to fetch contacts: {e}",
-#             )
-
-
-# @app.get("/contacts", response_class=HTMLResponse)
-# async def get_contacts(authorization: str = Depends()):
-#     if not authorization or not authorization.startswith("Bearer "):
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authorization header")
-
-#     access_token = authorization.split(" ")[1]
-
-#     async with httpx.AsyncClient() as client:
-#         try:
-#             response = await client.get(f"{base_url}/contacts/")
-#             response.raise_for_status()  # Перевірка статусу відповіді
-#             return {"message": "Contacts fetched successfully", "data": response.json()}
-#         except httpx.HTTPStatusError as e:
-#             raise Exception(f"Failed to fetch contacts: {e}")
-
-
-#     # Тут має бути ваша логіка для використання access_token у запиті до захищеного ендпоінту (наприклад, /contacts)
-
-#     return {"message": "Contacts retrieved successfully"}
