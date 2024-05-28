f1d045fd23d7c62d0b58d5f5e0791ac0f4fc4202
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-05-28 18:48:29 +0200

    I changes styles

--- static/script.js
+++ static/script.js
@@ -4,20 +4,6 @@ document.addEventListener('mousemove', (event) => {
     moveCursorGlow(x, y);
 });
 
-// function createStar(x, y) {
-//     const star = document.createElement('div');
-//     star.classList.add('star');
-//     const offsetX = window.pageXOffset;
-//     const offsetY = window.pageYOffset;
-    
-//     star.style.left = `${x + offsetX}px`;
-//     star.style.top = `${y + offsetY}px`;
-//     document.body.appendChild(star);
-//     star.addEventListener('animationend', () => {
-//         star.remove();
-//     });
-// }
-
 function createStar(x, y) {
     const numStars = 1;
 
@@ -55,59 +41,6 @@ function moveCursorGlow(x, y) {
     cursorGlow.style.top = `${y + offsetY}px`;
 }
 
-
-
-
-
-
-
-
-
-//   document.addEventListener('DOMContentLoaded', () => {
-//     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-
-//     const loginButton = document.getElementById('loginButton');
-//     const signupButton = document.getElementById('signupButton');
-//     const userButtons = document.getElementById('userButtons');
-
-//     if (isLoggedIn) {
-//         loginButton.style.display = 'none';
-//         signupButton.style.display = 'none';
-//         userButtons.style.display = 'block';
-//     } else {
-//         loginButton.style.display = 'block';
-//         signupButton.style.display = 'block';
-//         userButtons.style.display = 'none';
-//     }
-// });
-
-// function login() {
-//     localStorage.setItem('isLoggedIn', 'true');
-//     window.location.reload();
-// }
-
-// function logout() {
-//     localStorage.setItem('isLoggedIn', 'false');
-//     window.location.reload();
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.photo-card');
 
@@ -116,11 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
             const { clientX, clientY } = e;
             const { left, top, width, height } = card.getBoundingClientRect();
 
-            // Знаходимо положення курсора відносно середини карточки
             const xPos = (clientX - (left + width / 2)) / (width / 2);
             const yPos = (clientY - (top + height / 2)) / (height / 2);
 
-            // Обертаємо карточку з урахуванням положення курсора
             card.style.transform = `rotateY(${xPos * 10}deg) rotateX(${-yPos * 10}deg)`;
         });
 
